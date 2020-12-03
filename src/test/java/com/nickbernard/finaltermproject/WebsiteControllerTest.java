5065d5307110d852365064b28792bc1543386a1c
Nicholas Bernard <nbernard@albany.edu>
2020-12-03 17:05:54 -0500

    Added testing dependencies in pom file, created blank test

---
+++ src/test/java/com/nickbernard/finaltermproject/WebsiteControllerTest.java
@@ -0,0 +1,15 @@
+package com.nickbernard.finaltermproject;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class WebsiteControllerTest {
+
+    @Test
+    void getPageWithWeatherInfo() {
+
+
+    }
+}
